db3c38957e29cef7c5ff70f03f8d0eee4be45018
FAForhad <faforhad450@gmail.com>
2022-08-22 18:37:33 +0600

    added-function-conditions-css-design-more

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
+// find buttons
 const buttons = document.querySelectorAll('.btn-select');
+// push child's
 const playerArray = [];
 for (button of buttons) {
+    // added click event
     button.addEventListener('click', function (event) {
         const playerName = event.target.parentNode.parentNode.children[0].innerText;
         const playerList = document.getElementById('player-List');
         playerArray.push(playerName);
         const li = document.createElement('li')
         li.innerText = playerName;
-        li.setAttribute('class', 'list-unstyled p-4 text-white my-3');
-
+        li.setAttribute('class', 'fs-5 list-unstyled p-4 text-white my-3');
+        // apply conditions
         if (playerArray.length > 5) {
             alert('You Cannot select more than 5 players!')
             playerArray.pop();
@@ -19,21 +22,44 @@ for (button of buttons) {
         }
     })
 }
-
+    // added click event to the button
 document.getElementById('calculate-btn').addEventListener('click', function () {
-
+    // calculate value
     const player = getInputValueById('player')
     const expenses = player * playerArray.length;
-    const expansesField = setValueById('expanses-field', expenses);
-    return expansesField
-})
+        // apply conditions
+    if (player <= 0) {
+        alert('!!!! Input Number Please !!!!')
+        setValueById('expanses-field', 0);
 
+    }
+        // apply conditions isnan
+    else if (isNaN(player)) {
+        alert('!!!! Input Number Please !!!!')
+        setValueById('expanses-field', 0);
+    }
+    else {
+        setValueById('expanses-field', expenses);
+    }
+})
+    // added click event to the button
 document.getElementById('calculate-total-btn').addEventListener('click', function () {
+    // calculate value
     const expansesField = getElementTextById('expanses-field');
     const manager = getInputValueById('manager');
     const coach = getInputValueById('coach');
     const expanseTotal = expansesField + manager + coach;
-    const total = setValueById('total', expanseTotal);
-    return total;
-
+        // apply conditions 
+    if (manager <= 0 || coach <= 0) {
+            alert('!!!! Please Use Positive Input !!!!');
+            setValueById('total', 0);
+    }
+        // apply conditions isnan
+    else if (isNaN(manager) || isNaN(coach)) {
+        alert('!!!! Input Number Please !!!!')
+        setValueById('total', 0)
+    }
+    else {
+        setValueById('total', expanseTotal);
+    }
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,17 +1,28 @@
 
+// -------------------elements text---------------
+
 function getElementTextById(elementId) {
     const elementTextId = document.getElementById(elementId);
     const elementTextString = elementTextId.innerText;
+    // convert the string
     const elementText = parseFloat(elementTextString);
     return elementText;
 }
+
+// -------------------input value-------------------
+
 function getInputValueById(inputId) {
     const inputValueId = document.getElementById(inputId);
     const inputValueString = inputValueId.value;
+    // convert the string
     const inputValue = parseFloat(inputValueString);
     return inputValue;
 }
+
+// -------------------set value-------------------
+
 function setValueById(id, value) {
     const getValueId = document.getElementById(id);
+    // set value 
     getValueId.innerText = value;
 }
